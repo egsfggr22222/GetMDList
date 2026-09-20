3f3fcec7be55aadb0cd63860dba9a873614cf4d6
egsfggr22222 <3698009894@qq.com>
2026-09-21 03:30:55 +0800

    Update GetMDList.py

--- GetMDList.py
+++ GetMDList.py
@@ -59,6 +59,28 @@ TEXT_EXTENSIONS = {
     '.mk', '.cmake', '.ninja', '.gradle',
     '.sip',
 }
+
+
+TEXT_EXTENSIONS.update({
+    '.bat', '.cmd', '.vbs', '.vbe', '.wsf', '.wsh',
+    '.reg', '.inf', '.scf', '.jse',
+    '.asm', '.s', '.f', '.f77', '.f90', '.f95', '.f03', '.for', '.ftn',
+    '.pas', '.pp', '.p', '.inc', '.d', '.di', '.zig', '.zon',
+    '.svh', '.m', '.mm', '.swift', '.cs', '.csx', '.vb',
+    '.rs', '.rlib', '.hs', '.lhs', '.fsscript',
+    '.rbx', '.rjs', '.gemspec', '.rake', '.rakefile',
+    '.php3', '.php4', '.php5', '.phtml', '.ctp',
+    '.pyi', '.rpy', '.cpy', '.gyp', '.gypi',
+    '.wlua', '.coffee', '.rhistory', '.rprofile', '.rt',
+    '.nimble', '.clj', '.cljs', '.cljc', '.edn',
+    '.ss', '.sch', '.dats', '.sats', '.hats',
+    '.vams',
+    '.mak', '.bazel', '.bzl', '.buck', '.tf', '.tfvars', '.hcl', '.proto',
+    '.asc', '.asciidoc', '.adoc', '.rest', '.diff', '.patch',
+    '.mbox', '.eml', '.msg', '.svg', '.ipynb',
+    '.cql', '.cypher', '.graphql', '.gql', '.nsi', '.nsh', '.bicep',
+})
+}
 TEXT_FILENAMES = {'makefile', 'dockerfile', 'jenkinsfile', 'vagrantfile',
                   'rakefile', 'gemfile', 'procfile', 'brewfile'}
 
